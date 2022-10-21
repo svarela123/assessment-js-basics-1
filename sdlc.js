fe0ce0ad492545db1d8dc9c1c0e5ef47ed324873
Sergio Varela <varela.a.sergio@gmail.com>
2022-10-21 10:41:55 -0600

    Lifecycle definitions

---
+++ sdlc.js
@@ -0,0 +1,37 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+let analysisOfRequirements = "The process of collating factual data, identify software flaws, understanding the information architecture and developing solutions to overcome the weaknesses of the project."
+
+let design = "Design the architecture of the software."
+
+let implementation = "Build the code, test, integrate and manage."
+
+let testingAndIntegration = "Testing the written code for bugs and other inconsistencies."
+
+let maintain = "The project team updates software to make sure it is relevant to the current market needs."
